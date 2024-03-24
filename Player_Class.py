70fdac72e3bd8b4941c20cc29474908616c020e8
Akshat Harlalka <akshatharlalka.ah@icloud.com>
2024-03-24 16:37:07 -0400

    player created

--- Player_Class.py
+++ Player_Class.py
@@ -3,9 +3,11 @@
 This Class represent a player in a game
 """
 class Player:
-    def __init__(self, name, money) -> None:
+    def __init__(self, name, money, type) -> None:
         self.name = name
         self.money = money
+        self.status = 0
+        self.type = type
     
     def bet(self, money):
         if money > self.money:
@@ -19,3 +21,21 @@ class Player:
     
     def get_infomation(self):
         return [self.name, self.money]
+    
+    def change_status(self, status):
+        """
+        fold = 0
+        in_game = 1
+        the game would keep track of rasies and blinds
+        """
+        self.status = status
+    
+    def get_status(self):
+        return self.status
+
+    def get_type(self):
+        """
+        0: human
+        1: AI
+        """
+        return self.type
